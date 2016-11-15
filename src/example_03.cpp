9e7bd5195cb99619111eccb7ff11781b46212f21
Claire Asselstine <claire.asselstine@gmail.com>
2016-11-15 13:04:49 -0800

    draw blue box

--- src/example_03.cpp
+++ src/example_03.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
-
+#include <glm/vec3.hpp>
 //include header file for glfw library so that we can use OpenGL
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-
+#include "example_03.h"
 #ifdef _WIN32
 static DWORD lastTime;
 #else
@@ -31,8 +31,81 @@ bool auto_strech = false;
 int Width_global = 400;
 int Height_global = 400;
 int Z_buffer_bit_depth = 128;
+vector< vector < vector < glm::vec3>>> patches; // Patches data structure [# patches][4][4][xyz point]
+vector<shape> shapes; // Shapes (either triangle or quad)
+bool is_adaptive; // adaptive or uniform subdivision
+bool is_smooth_shade; // If not smooth, then flat shading
+float step_size;
+int num_steps;
+bool wireframe_mode;
+bool filled_mode;
+float epsilon;
 
 inline float sqr(float x) { return x*x; }
+
+
+
+// ****************
+//  PARSE 
+//  Parses a .bez file, and populates the global patches vector
+//  ***************
+void parse_file(char* filename){
+}
+
+// ****************
+// CREATE SHAPES
+// Given the current patches, populate the global shapes vector
+// Depending on is_adaptive, shapes will either be a vector of triangles 
+// or a vector of squares
+// ****************
+void create_shapes(void){
+}
+
+// ****************
+// ADAPTIVE SUBDIVISION
+// Given a single patch and an epsilon, creates the list of triangles which subdivide the patch
+// such that no point is further than epsilon away from the surface of the bezier curve
+// This entire list is pushed back onto the global list of patches 
+// ****************
+void adaptive_subdivision(vector<vector<glm::vec3>> patch){
+}
+
+// ****************
+// UNIFORM SUBDIVISION
+// Given a single patch, step size, and num_steps, creates list of squares that are
+// sufficiently small, and pushes them back on to the global list of shapes
+// ****************
+void uniform_subdivision(vector<vector<glm::vec3>> patch, float step_size, int num_steps){
+}
+
+
+// *****************
+// TRIANGULATE 
+// Given a patch and three (u,v) pairs, and global epsilon, push back onto global shape list
+// *****************
+void triangulate(vector<vector<glm::vec3>> patch, float p1[2], float p2[2], float p3[2]){
+}
+
+// ****************
+// BEZ PATCH INTERPOLATION
+// Given a patch and a point defined by (u,v)
+// finds the value of the bezier surface at this point
+// also finds the normal at this point
+// ****************
+glm::vec3 patch_interp(vector<vector<glm::vec3>> patch, float u, float v, glm::vec3 * norm) {
+    glm::vec3 point = glm::vec3(0,0,0);
+    return point;
+}
+
+// *****************
+// BEZ CURVE INTERPOLATION
+// Given a curve of four control points, finds the bezier interpolation on this curve and
+// evaluates at a given u value, and finds the derivative
+// *****************
+glm::vec3 curve_interp(vector<glm::vec3> curve, float param, float *derivative) {
+    glm::vec3 point = glm::vec3(0,0,0);
+    return point; 
+}
 
 
 //****************************************************
@@ -81,56 +154,86 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     
 }
 
+void drawShapes(){
+
+    glBegin(GL_QUADS);
+    for(shape s: shapes) {
+        glColor3f(0.0f, 0.0f, 1.0f);
+        for(glm::vec3 v : s.vertices) {
+            glVertex3f(v[0], v[1], v[2]);
+        }
+    }
+
+    glEnd();
+
+    
+
+}
+
 //****************************************************
 // Draw a cube. You don't need this for your final assignment, but it's
 // here so you don't look at a blank screen.
 // Taken from https://www.ntu.edu.sg/home/ehchua/programming/opengl/CG_Examples.html
 //****************************************************
 void drawCube() {
-	 glBegin(GL_QUADS);                // Begin drawing the color cube with 6 quads
+    
+    shapes = vector<shape>();
+    shape shape1 = shape();
+    
+    
+             // Begin drawing the color cube with 6 quads
       // Top face (y = 1.0f)
       // Define vertices in counter-clockwise (CCW) order with normal pointing out
-      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-      glVertex3f( 1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f,  1.0f);
-      glVertex3f( 1.0f, 1.0f,  1.0f);
- 
+
+    shape1.vertices.push_back(glm::vec3( 1.0f, 1.0f, -1.0f));
+    shape1.vertices.push_back(glm::vec3(-1.0f, 1.0f, -1.0f));
+    shape1.vertices.push_back(glm::vec3(-1.0f, 1.0f,  1.0f));
+    shape1.vertices.push_back(glm::vec3( 1.0f, 1.0f,  1.0f));
+    shapes.push_back(shape1);
       // Bottom face (y = -1.0f)
-      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-      glVertex3f( 1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f( 1.0f, -1.0f, -1.0f);
  
+    shape shape2 = shape();
+    shape2.vertices.push_back(glm::vec3( 1.0f, -1.0f,  1.0f));
+    shape2.vertices.push_back(glm::vec3(-1.0f, -1.0f,  1.0f));
+    shape2.vertices.push_back(glm::vec3(-1.0f, -1.0f, -1.0f));
+    shape2.vertices.push_back(glm::vec3( 1.0f, -1.0f, -1.0f));
+    shapes.push_back(shape2);
+
+    shape shape3 = shape();    
       // Front face  (z = 1.0f)
-      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-      glVertex3f( 1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f, -1.0f, 1.0f);
-      glVertex3f( 1.0f, -1.0f, 1.0f);
- 
+    shape3.vertices.push_back(glm::vec3( 1.0f,  1.0f, 1.0f));
+    shape3.vertices.push_back(glm::vec3(-1.0f,  1.0f, 1.0f));
+    shape3.vertices.push_back(glm::vec3(-1.0f, -1.0f, 1.0f));
+    shape3.vertices.push_back(glm::vec3(1.0f, -1.0f, 1.0f));
+    shapes.push_back(shape3);
+
+    shape shape4 = shape(); 
       // Back face (z = -1.0f)
-      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f( 1.0f,  1.0f, -1.0f);
+    shape4.vertices.push_back(glm::vec3 (1.0f, -1.0f, -1.0f));
+     shape4.vertices.push_back(glm::vec3 (-1.0f, -1.0f, -1.0f));
+     shape4.vertices.push_back(glm::vec3 (-1.0f,  1.0f, -1.0f));
+     shape4.vertices.push_back(glm::vec3 ( 1.0f,  1.0f, -1.0f));
  
+    shapes.push_back(shape4);
+    shape shape5 = shape();
+    
       // Left face (x = -1.0f)
-      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-      glVertex3f(-1.0f,  1.0f,  1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
+      shape5.vertices.push_back(glm::vec3(-1.0f,  1.0f,  1.0f));
+       shape5.vertices.push_back(glm::vec3(-1.0f,  1.0f, -1.0f));
+       shape5.vertices.push_back(glm::vec3(-1.0f, -1.0f, -1.0f));
+       shape5.vertices.push_back(glm::vec3(-1.0f, -1.0f,  1.0f));
  
+    shapes.push_back(shape5);
+    shape shape6 = shape();
+    
       // Right face (x = 1.0f)
-      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-      glVertex3f(1.0f,  1.0f, -1.0f);
-      glVertex3f(1.0f,  1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f, -1.0f);
-   glEnd();  // End of drawing color-cube
+      shape6.vertices.push_back(glm::vec3(1.0f,  1.0f, -1.0f));
+      shape6.vertices.push_back(glm::vec3(1.0f,  1.0f,  1.0f));
+      shape6.vertices.push_back(glm::vec3(1.0f, -1.0f,  1.0f));
+      shape6.vertices.push_back(glm::vec3(1.0f, -1.0f, -1.0f));
+    
+    shapes.push_back(shape6);
+    drawShapes();
 }
 
 
